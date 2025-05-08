21a59f117afa2d85c0bd03a15fb88ea9068c8a5d
Edvin <edvinullberg@outlook.com>
2025-05-08 23:04:24 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React-todo/',  // viktigt: måste matcha repo-namnet
-  plugins: [react()],
+  base: '/React-todo-gamification/',  // 👈 viktigt!
+  plugins: [react()]
 })
